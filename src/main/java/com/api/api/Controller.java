601b917e9474eeb9243f36102405ac2cf769f128
fjhernandez <fjhernandez@quarksoft.net>
2021-10-07 12:56:22 -0500

    Minior Update

--- src/main/java/com/api/api/Controller.java
+++ src/main/java/com/api/api/Controller.java
@@ -8,29 +8,32 @@ import java.util.List;
 
 @RestController
 public class Controller {
+
+    List<Dog> dogs = new ArrayList<>();
     
     @GetMapping("/")
     @ResponseStatus( HttpStatus.OK )
     public String home(){
+        if(dogs.isEmpty()) {
+            Dog dog1 = new Dog("Taffy", "Blanco");
+            Dog dog2 = new Dog("Pucho", "Blanco");
+            Dog dog3 = new Dog("El Orejas", "Blanco/Cafe");
+            dogs.add(dog1);
+            dogs.add(dog2);
+            dogs.add(dog3);
+        }
+        
         return "Dogs API";
     }
 
     @GetMapping("/dogs")
     public List<Dog> getDogs(){
-        Dog dog1 = new Dog("Taffy", "Blanco");
-        Dog dog2 = new Dog("Pucho", "Blanco");
-        Dog dog3 = new Dog("El Orejas", "Blanco/Cafe");
-
-        List<Dog> dogs = new ArrayList<>();
-        dogs.add(dog1);
-        dogs.add(dog2);
-        dogs.add(dog3);
-
         return dogs;
     }
 
     @PostMapping("/dogs")
     public Dog addDog(@RequestBody Dog dog){
+        dogs.add(dog);
         return dog;
     }
 }
